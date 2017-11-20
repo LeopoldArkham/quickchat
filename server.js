cc77f961c6fb5c2fa1c45a785da36a2a9c62fa3e
LeopoldArkham <leopold.arkham@gmail.com>
2017-11-20 14:44:20 +0100

    Add index page

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ var io = require('socket.io')(http);
 var connected = 0;
 var rooms = [];
 
-// app.get('/', function(req, res) {
-//     res.sendFile(__dirname + '/index.html');
-// });
+// Serve root
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
 
-// Serve HTML
+// Serve room
 app.get('/rooms/:roomID', function(req, res) {
     res.sendFile(__dirname + '/chatroom.html');
 })
@@ -21,29 +22,28 @@ io.on('connection', function(socket){
     // Joining/creating a room
     socket.on('join', function(room, username) {
         // Track number of users in room        
-        if (room in rooms) {
-            rooms[room]++;
-        } else {
-            rooms[room] = 1;
+        if (!(room in rooms)) {
+            rooms[room] = new Room(room, "");
         }
+        rooms[room].userConnected();
         
         // Propagate number of users
         socket.join(room);
-        io.to(room).emit('nb_users', rooms[room]);
+        io.to(room).emit('nb_users', rooms[room].connectedUsers);
         // Persist rom ID and username on the socket itself
         socket.room = room;
         socket.username = username;
 
         // Handle disconnects
         socket.on('disconnect', function () {
-            rooms[room]--;
-            io.to(room).emit('nb_users', rooms[room]);
+            rooms[room].users--;
+            io.to(room).emit('nb_users', rooms[room].connectedUsers);
         })
     });
 
     // Propagate recieved messages
     socket.on('msg', function(_msg){
-      io.to(socket.room).emit('msg', socket.username.length == 0? "Default":socket.username, _msg);
+      io.to(socket.room).emit('msg', socket.username, _msg);
     });
   });
 
